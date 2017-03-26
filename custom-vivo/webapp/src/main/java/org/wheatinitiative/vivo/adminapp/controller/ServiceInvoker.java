8e3b3da299cdbcbfde754859890562c9d7a6b94a
Brian Lowe <brian@ontocale.com>
2017-03-26 15:09:00 +0300

    Move start/stop behavior to DataSourceScheduler

--- custom-vivo/webapp/src/main/java/org/wheatinitiative/vivo/adminapp/controller/ServiceInvoker.java
+++ custom-vivo/webapp/src/main/java/org/wheatinitiative/vivo/adminapp/controller/ServiceInvoker.java
@@ -2,17 +2,16 @@ package org.wheatinitiative.vivo.adminapp.controller;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.concurrent.ScheduledFuture;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.utils.URIBuilder;
-import org.wheatinitiative.vivo.adminapp.datasource.DataSourceManager;
+import org.wheatinitiative.vivo.adminapp.datasource.DataSourceScheduler;
 import org.wheatinitiative.vivo.adminapp.datasource.RDFServiceModelConstructor;
-import org.wheatinitiative.vivo.datasource.DataSourceDescription;
 import org.wheatinitiative.vivo.datasource.dao.DataSourceDao;
-import org.wheatinitiative.vivo.datasource.service.DataSourceDescriptionSerializer;
 import org.wheatinitiative.vivo.datasource.util.http.HttpUtils;
 
 import edu.cornell.mannlib.vitro.webapp.auth.permissions.SimplePermission;
@@ -26,7 +25,6 @@ public class ServiceInvoker extends FreemarkerHttpServlet {
 
     private static final Log log = LogFactory.getLog(ServiceInvoker.class);
     private static final String REDIRECT_PAGE = "/listDataSources";
-    private HttpUtils httpUtils = new HttpUtils();
     protected static final AuthorizationRequest REQUIRED_ACTIONS = 
             SimplePermission.USE_MISCELLANEOUS_ADMIN_PAGES.ACTION;
     
@@ -34,36 +32,20 @@ public class ServiceInvoker extends FreemarkerHttpServlet {
     protected AuthorizationRequest requiredActions(VitroRequest vreq) {
         return REQUIRED_ACTIONS;
     }
-    
-    // It would be awfully nice to send an error if someone tries to GET this 
-    // servlet, but it would mean overriding everything in FreemarkerHttpServlet,
-    // which implements doGet() and then calls
-    // doGet() from doPost(), instead of putting the processing in a separate 
-    // method and calling it from each.
-//    @Override 
-//    public void doGet(HttpServletRequest request, HttpServletResponse response) 
-//            throws IOException {
-//        // GETting this servlet is not allowed
-//        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-//    }
-    
+        
     @Override
     public ResponseValues processRequest(VitroRequest request) 
             throws IOException, URISyntaxException {
         VitroRequest vreq = new VitroRequest(request);
-        DataSourceDao dsm = new DataSourceDao(
-                new RDFServiceModelConstructor(vreq.getRDFService()));
         String dataSourceURI = vreq.getParameter("uri");
-        DataSourceDescription dataSource = dsm.getDataSource(dataSourceURI);
-        String deploymentURL = dataSource.getConfiguration().getDeploymentURI();
-        if(deploymentURL != null) {
-            if(vreq.getParameter("start") != null) {
-                log.info("Starting data source");
-                this.startService(dataSource);
-            } else if (vreq.getParameter("stop") != null) {
-                log.info("Stopping data source");
-                this.stopService(dataSource);
-            }
+        DataSourceScheduler scheduler = DataSourceScheduler.getInstance(
+                getServletContext());
+        if(vreq.getParameter("start") != null) {
+            log.info("Starting data source");
+            scheduler.startNow(dataSourceURI);
+        } else if (vreq.getParameter("stop") != null) {
+            log.info("Stopping data source");
+            scheduler.stopNow(dataSourceURI);
         }
         URIBuilder locationB = new URIBuilder(REDIRECT_PAGE);
         String type = vreq.getParameter("type");
@@ -72,34 +54,6 @@ public class ServiceInvoker extends FreemarkerHttpServlet {
         }
         return new RedirectResponseValues(locationB.toString(), 
                 HttpServletResponse.SC_SEE_OTHER);
-    }
-    
-    private DataSourceDescription startService(DataSourceDescription description) {
-        description.getStatus().setRunning(true);
-        return updateService(description.getConfiguration().getDeploymentURI(),
-                description);
-    }
-    
-    private DataSourceDescription stopService(DataSourceDescription description) {
-        description.getStatus().setRunning(false);
-        return updateService(description.getConfiguration().getDeploymentURI(),
-                description);         
-    }
-    
-    private DataSourceDescription updateService(
-            String serviceURL, DataSourceDescription description) {
-        DataSourceDescriptionSerializer serializer = 
-                new DataSourceDescriptionSerializer();
-        String json = serializer.serialize(description);
-        String result = httpUtils.getHttpPostResponse(
-                serviceURL, json, "application/json");
-        try {
-            DataSourceDescription desc = serializer.unserialize(result);
-            return desc;
-        } catch (Exception e) {
-            throw new RuntimeException(
-                    "Exception parsing response from " + serviceURL + ": \n" + result);
-        }
     }
     
 }
--- custom-vivo/webapp/src/main/java/org/wheatinitiative/vivo/adminapp/datasource/DataSourceScheduler.java
+++ custom-vivo/webapp/src/main/java/org/wheatinitiative/vivo/adminapp/datasource/DataSourceScheduler.java
@@ -3,46 +3,64 @@ package org.wheatinitiative.vivo.adminapp.datasource;
 import java.util.HashMap;
 import java.util.concurrent.ScheduledFuture;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.wheatinitiative.vivo.datasource.DataSourceDescription;
+import org.wheatinitiative.vivo.datasource.dao.DataSourceDao;
+import org.wheatinitiative.vivo.datasource.service.DataSourceDescriptionSerializer;
+import org.wheatinitiative.vivo.datasource.util.http.HttpUtils;
 
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
 import edu.cornell.mannlib.vitro.webapp.rdfservice.ChangeListener;
 import edu.cornell.mannlib.vitro.webapp.rdfservice.ModelChange;
+import edu.cornell.mannlib.vitro.webapp.rdfservice.RDFService;
 import edu.cornell.mannlib.vitro.webapp.rdfservice.RDFServiceException;
 import edu.cornell.mannlib.vitro.webapp.rdfservice.impl.RDFServiceUtils;
+import edu.cornell.mannlib.vitro.webapp.utils.threads.VitroBackgroundThread;
 
 public class DataSourceScheduler implements ServletContextListener, ChangeListener {
 
-    ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-
-    private HashMap<String, DataSourceTask> uriToTask = 
-            new HashMap<String, DataSourceTask>();
-    private HashMap<DataSourceTask, ScheduledFuture<?>> taskToFuture =  
-            new HashMap<DataSourceTask, ScheduledFuture<?>>();
-
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        try {
-            for(ScheduledFuture<?> f : this.taskToFuture.values()) {
-                f.cancel(true);        
-            }
-            scheduler.shutdown();
-            scheduler.destroy();
-            sce.getServletContext().setAttribute(this.getClass().getName(), null);
-            RDFServiceUtils.getRDFServiceFactory(
-                    sce.getServletContext()).unregisterListener(this);
-        } catch (Exception e) {
-            // ignore for now - possible NPEs as things are destroyed
+    private static final Log log = LogFactory.getLog(DataSourceScheduler.class);
+    
+    private ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+    private DataSourceDao dataSourceDao;
+    private RDFService rdfService;
+    private HttpUtils httpUtils = new HttpUtils();
+    
+    private HashMap<String, ScheduledFuture<?>> uriToFuture =  
+            new HashMap<String, ScheduledFuture<?>>();
+    
+    public static DataSourceScheduler getInstance(ServletContext ctx) {
+        Object o = ctx.getAttribute(DataSourceScheduler.class.getName());
+        if (o instanceof DataSourceScheduler) {
+            return (DataSourceScheduler) o;
+        } else {
+            return null;
         }
     }
-
+    
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        try {
+            this.rdfService = ModelAccess.on(
+                    sce.getServletContext()).getRDFService();
+            this.dataSourceDao = new DataSourceDao(
+                    new RDFServiceModelConstructor(this.rdfService));
+        } catch (Exception e) {
+            throw new RuntimeException(this.getClass().getSimpleName() 
+                    + " must be run after the context's RDFService is set up.");
+        }
         scheduler.setPoolSize(20);
+        scheduler.setThreadFactory(
+                new VitroBackgroundThread.Factory("DataSourceTask"));
         scheduler.initialize();
-        sce.getServletContext().setAttribute(this.getClass().getName(), this);
+        sce.getServletContext().setAttribute(
+                DataSourceScheduler.class.getName(), this);
         try {
             RDFServiceUtils.getRDFServiceFactory(
                     sce.getServletContext()).registerListener(this);
@@ -50,15 +68,122 @@ public class DataSourceScheduler implements ServletContextListener, ChangeListen
             throw new RuntimeException(e);
         }
         scheduleTestTask();
+        // TODO scheduleAllTasks();
     }
-
+    
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        try {
+            for(ScheduledFuture<?> f : this.uriToFuture.values()) {
+                f.cancel(true);        
+            }
+            scheduler.shutdown();
+            scheduler.destroy();
+            sce.getServletContext().setAttribute(
+                    this.getClass().getName(), null);
+            RDFServiceUtils.getRDFServiceFactory(
+                    sce.getServletContext()).unregisterListener(this);
+        } catch (Exception e) {
+            // ignore for now - possible NPEs as things are destroyed
+        }
+    }
+    
     private void scheduleTestTask() {
-        DataSourceTask testTask = new DataSourceTask();
-        this.uriToTask.put("http://example.com/testTask", testTask);
-        this.taskToFuture.put(testTask, scheduler.scheduleAtFixedRate(
-                new DataSourceTask(), 10000 /* ms */));
+        this.uriToFuture.put("http://example.com/testTask", 
+                scheduler.scheduleAtFixedRate(new TestTask(), 10000 /* ms */));
+    }
+        
+    public void startNow(String dataSourceURI) {
+        new DataSourceStarter(dataSourceURI).run();
+    }
+    
+    public void stopNow(String dataSourceURI) {
+        new DataSourceStopper(dataSourceURI).run();
+    }
+    
+    public void startAsync(String dataSourceURI) {
+        scheduler.execute(new DataSourceStarter(dataSourceURI));
+    }
+    
+    public void stopSync(String dataSourceURI) {
+        scheduler.execute(new DataSourceStopper(dataSourceURI));
+    }
+    
+    private void cancel(String dataSourceURI) {
+        ScheduledFuture<?> future = this.uriToFuture.get(dataSourceURI);
+        if(future != null) {
+            future.cancel(true);
+        }
+        this.uriToFuture.put(dataSourceURI, null);
     }
 
+    private DataSourceDescription getDataSourceDescription(
+            String dataSourceURI) {
+        DataSourceDescription ds = this.dataSourceDao.getDataSource(
+                dataSourceURI);
+        if(ds == null) {
+            throw new RuntimeException("DataSource " + dataSourceURI 
+                    + "not found");
+        }
+        return ds;
+    }
+    
+    private class DataSourceStarter implements Runnable {
+
+        private String dataSourceURI;
+        
+        public DataSourceStarter(String dataSourceURI) {
+            this.dataSourceURI = dataSourceURI;
+        }
+        
+        @Override
+        public void run() {
+            DataSourceDescription desc = getDataSourceDescription(
+                    dataSourceURI);            
+            desc.getStatus().setRunning(true);
+            updateService(desc.getConfiguration().getDeploymentURI(), desc);            
+        }
+        
+    }
+    
+    private class DataSourceStopper implements Runnable {
+
+        private String dataSourceURI;
+        
+        public DataSourceStopper(String dataSourceURI) {
+            this.dataSourceURI = dataSourceURI;
+        }
+        
+        @Override
+        public void run() {
+            DataSourceDescription desc = getDataSourceDescription(
+                    dataSourceURI);            
+            desc.getStatus().setRunning(false);
+            updateService(desc.getConfiguration().getDeploymentURI(), desc);            
+        }
+        
+    }
+    
+    protected DataSourceDescription updateService(
+            String deploymentURL, DataSourceDescription description) {
+        if(deploymentURL == null) {
+            throw new RuntimeException("deployment URL may not be null");
+        }
+        DataSourceDescriptionSerializer serializer = 
+                new DataSourceDescriptionSerializer();
+        String json = serializer.serialize(description);
+        String result = httpUtils.getHttpPostResponse(
+                deploymentURL, json, "application/json");
+        try {
+            DataSourceDescription desc = serializer.unserialize(result);
+            return desc;
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Exception parsing response from " + deploymentURL 
+                            + ": \n" + result);
+        }
+    }
+    
     @Override
     public void notifyEvent(String arg0, Object arg1) {
         // TODO Auto-generated method stub
@@ -66,7 +191,14 @@ public class DataSourceScheduler implements ServletContextListener, ChangeListen
 
     @Override
     public void notifyModelChange(ModelChange arg0) {
-        // TODO Auto-generated method stub        
+        // TODO listen for changes and schedule / cancel tasks as necessary        
+    }
+    
+    public class TestTask implements Runnable {
+        @Override
+        public void run() {
+            log.info("Hi there; I'm a scheduled task, but I'm not doing very much.");
+        }
     }
 
 }
