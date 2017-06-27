e1f0416a322b2fbefcdc9f4cec10a4fd3ec7aea4
Brian Lowe <brian@ontocale.com>
2017-06-27 18:18:27 +0300

    Add servlet to export faux property contexts for use by merge source

---
+++ custom-vivo/webapp/src/main/java/org/wheatinitiative/vivo/adminapp/controller/FauxPropertyContextExportController.java
@@ -0,0 +1,53 @@
+package org.wheatinitiative.vivo.adminapp.controller;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
+
+/**
+ * Outputs the faux property context RDF from the display model so that
+ * services (such as merge services) can make use of it in addition to the
+ * normal RDF that's available in the main triple store.
+ * @author Brian Lowe
+ *
+ */
+public class FauxPropertyContextExportController extends HttpServlet {
+
+    private static final String APPLICATION_CONTEXT_NS = "http://vitro.mannlib.cornell.edu/ns/vitro/ApplicationConfiguration#";
+    private final Log log = LogFactory.getLog(FauxPropertyContextExportController.class);
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws IOException {
+        VitroRequest vreq = new VitroRequest(request);    
+        Model displayModel = vreq.getDisplayModel();
+        Model contexts = ModelFactory.createDefaultModel();
+        String queryStr = "DESCRIBE ?x WHERE { \n" 
+                + "    ?x a <" + APPLICATION_CONTEXT_NS + "ConfigContext> \n"  
+                + "} \n";
+        QueryExecution qe = QueryExecutionFactory.create(queryStr, displayModel);
+        try {
+            qe.execDescribe(contexts);
+            OutputStream os = response.getOutputStream();
+            response.setContentType("text/turtle");
+            contexts.write(os, "TTL");
+            os.flush();
+        } finally {
+            qe.close();
+        }
+    }
+    
+}
